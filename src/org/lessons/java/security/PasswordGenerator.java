ec0240de7d74fb5a1cdbc5923cad7b42e86b681e
FrancescoGuggino <landongu@gmail.com>
2023-07-12 14:15:07 +0200

    add variables 2

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -3,14 +3,16 @@ package org.lessons.java.security;
 public class PasswordGenerator {
     public static void main(String[] args) {
 
-        //salvare in opportune variabili il nome, cognome, colore preferito e data di nascita di un utente
+        // salvare in opportune variabili il nome, cognome, colore preferito e data di nascita di un utente
         // suddivisa in giorno, mese e anno (in numero)
         // Variabili
         String userName;
         String userSurname;
         String favoriteColor;
-        String dayBirth;
-        String monthBirth;
-        String yearBirth;
+        int dayBirth;
+        int monthBirth;
+        int yearBirth;
+        // assegno valori alle variabili
+
     }
 }

de96fd775f8e4187ac7ae1edec582a878f4ee4b6
FrancescoGuggino <landongu@gmail.com>
2023-07-12 14:58:06 +0200

    bonus. add var

--- src/org/lessons/java/security/BonusPasswordGenerator.java
+++ src/org/lessons/java/security/BonusPasswordGenerator.java
@@ -1,4 +1,23 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class BonusPasswordGenerator {
+    public static void main(String[] args) {
+
+        //dichiaro scanner
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Variabili
+        String userName;
+        String userSurname;
+        String favoriteColor;
+        int dayBirth;
+        int monthBirth;
+        int yearBirth;
+
+        //chiedo valori a utente
+
+    }
 }
